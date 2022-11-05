bfb20b11ed829d67ffd60933ee425e9d54827049
Ahmed Hammad <ahmed.hammad.am@gmail.com>
2022-11-05 18:15:53 +0100

    adding session table

--- products_api/api/models.py
+++ products_api/api/models.py
@@ -1,49 +1,20 @@
+from api.utils import NoLeadingZerosCharField
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from model_utils.models import TimeStampedModel
 
-from api.utils import NoLeadingZerosCharField
 
-
-class Item(TimeStampedModel):
-    db_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Database ID')
-    id = models.CharField(_('Item ID'), help_text='This is not the database id', max_length=225, null=True, blank=True)
-    code = NoLeadingZerosCharField(_('Code'), max_length=225, db_index=True, null=True, blank=True)
-    type = models.CharField(_('Type'), max_length=225, null=True, blank=True)
-    brand = models.CharField(_('Brand'), max_length=225, null=True, blank=True)
-    description = models.CharField(_('Description'), max_length=225, null=True, blank=True)
-    status = models.CharField(_('Status'), max_length=225, null=True, blank=True)
-    categ_id = models.IntegerField(_('Categ ID'), null=True, blank=True)
-    category_id = models.CharField(_('Category ID'), max_length=225, null=True, blank=True)
-    amount_multiplier = models.IntegerField(_('Amount Multiplier'), null=True, blank=True)
-    edeka_article_number = models.CharField(_('Edeka Article Number'), max_length=225, null=True, blank=True)
-    gross_weight = models.JSONField(_('Gross Weight'), null=True, blank=True)
-    net_weight = models.JSONField(_('Net Weight'), null=True, blank=True)
-    unit_name = models.CharField(_('Unit Name'), max_length=50, null=True, blank=True)
-    notes = models.CharField(_('Notes'), max_length=225, null=True, blank=True)
-    packaging = models.CharField(_('Packaging'), max_length=100, null=True, blank=True)
-    requires_best_before_date = models.BooleanField(_('Requires Best Before Date'), null=True, blank=True)
-    requires_meat_info = models.BooleanField(_('Requires Meat Info'), null=True, blank=True)
-    trade_item_unit_descriptor = models.CharField(_('Trade Item Unit Descriptor'), max_length=225, null=True, blank=True)
-    trade_item_unit_descriptor_name = models.CharField(_('Trade Item Unit Descriptor Name'), max_length=225, null=True, blank=True)
-    validation_status = models.CharField(_('Validation Status'), max_length=100, null=True, blank=True)
-    regulated_name = models.CharField(_('Regulated Name'), max_length=100, null=True, blank=True)
-    vat_rate = models.CharField(_('Vat Rate'), max_length=100, null=True, blank=True)
-    category = models.CharField(_('Category'), max_length=225, null=True, blank=True)
-    hierarchies = models.JSONField(_('Hierarchies'), null=True, blank=True)
-    vat = models.JSONField(_('vat'), null=True, blank=True)
-
-    class Meta:
-        verbose_name = _('Item')
-        verbose_name_plural = _('Items')
-        unique_together = ['code', 'type']
-
-    def _str__(self):
-        return self.code
+class Session(TimeStampedModel):
+    session_end_time = models.DateTimeField(_('Session End Time'), null=True, blank=True)
+    session_id = models.BigIntegerField(_('Session ID'), null=True, blank=True)
+    session_start_time = models.DateTimeField(_('Session Start Time'), null=True, blank=True)
+    supplier_id = models.CharField(_('Supplier id'), max_length=225, null=True, blank=True)
+    user_id = models.CharField(_('User id'), max_length=225, null=True, blank=True)
 
 
 class Lot(TimeStampedModel):
-    item = models.ForeignKey(Item, related_name='lot', on_delete=models.CASCADE, null=True, blank=True)
+    session = models.ForeignKey('Session', related_name='lot', on_delete=models.CASCADE, null=True, blank=True)
+    item = models.ForeignKey('Item', related_name='lot', on_delete=models.CASCADE, null=True, blank=True)
     lot_number = models.CharField(_('Lot Number'), max_length=225, null=True, blank=True)
     amount = models.IntegerField(_('Amount'), null=True, blank=True)
     bbd = models.DateTimeField(_('Best Before Date'), null=True, blank=True)
@@ -62,9 +33,46 @@ class Lot(TimeStampedModel):
         return self.bbd
 
 
+class Item(TimeStampedModel):
+    db_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Database ID')
+    id = models.CharField(_('Item ID'), help_text='This is not the database id', max_length=225, null=True, blank=True)
+    code = NoLeadingZerosCharField(_('Code'), max_length=225, db_index=True, null=True, blank=True)
+    type = models.CharField(_('Type'), max_length=225, null=True, blank=True)
+    brand = models.CharField(_('Brand'), max_length=225, null=True, blank=True)
+    description = models.CharField(_('Description'), max_length=225, null=True, blank=True)
+    status = models.CharField(_('Status'), max_length=225, null=True, blank=True)
+    categ_id = models.IntegerField(_('Categ ID'), null=True, blank=True)
+    category_id = models.CharField(_('Category ID'), max_length=225, null=True, blank=True)
+    amount_multiplier = models.IntegerField(_('Amount Multiplier'), null=True, blank=True)
+    edeka_article_number = models.CharField(_('Edeka Article Number'), max_length=225, null=True, blank=True)
+    gross_weight = models.JSONField(_('Gross Weight'), null=True, blank=True)
+    net_weight = models.JSONField(null=True, blank=True)
+    unit_name = models.CharField(_('Unit Name'), max_length=50, null=True, blank=True)
+    notes = models.CharField(_('Notes'), max_length=225, null=True, blank=True)
+    packaging = models.CharField(_('Packaging'), max_length=100, null=True, blank=True)
+    requires_best_before_date = models.BooleanField(_('Requires Best Before Date'), null=True, blank=True)
+    requires_meat_info = models.BooleanField(_('Requires Meat Info'), null=True, blank=True)
+    trade_item_unit_descriptor = models.CharField(_('Trade Item Unit Descriptor'), max_length=225, null=True, blank=True)
+    trade_item_unit_descriptor_name = models.CharField(_('Trade Item Unit Descriptor Name'), max_length=225, null=True, blank=True)
+    validation_status = models.CharField(_('Validation Status'), max_length=100, null=True, blank=True)
+    regulated_name = models.CharField(_('Regulated Name'), max_length=100, null=True, blank=True)
+    vat_rate = models.CharField(_('Vat Rate'), max_length=100, null=True, blank=True)
+    category = models.CharField(_('Category'), max_length=225, null=True, blank=True)
+    hierarchies = models.JSONField(null=True, blank=True)
+    vat = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = _('Item')
+        verbose_name_plural = _('Items')
+        # unique_together = ['code', 'type']
+
+    def _str__(self):
+        return self.code
+
+
 class RelatedProduct(TimeStampedModel):
-    item = models.ForeignKey(Item, related_name='related_products', on_delete=models.CASCADE)
-    gtin = models.CharField(_('Gtin'), max_length=225, null=True, blank=True)
+    item = models.ForeignKey('Item', related_name='item', on_delete=models.CASCADE)
+    gtin = models.CharField(max_length=225, null=True, blank=True)
     trade_item_unit_descriptor = models.CharField(_('Trade Item Unit Descriptor'), max_length=225, null=True, blank=True)
 
     class Meta:
--- products_api/api/views.py
+++ products_api/api/views.py
@@ -1,35 +1,63 @@
 
 import json
 
+from api.models import Item
+from api.serializers import (AcceptCodeSerializer, AcceptFileSerializer,
+                             ItemListSerializer, LotCreateSerializer,
+                             SessionCreateSerializer)
+from api.utils import remove_leading_zeros
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.parsers import JSONParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from api.models import Item
-from api.serializers import (AcceptCodeSerializer, AcceptFileSerializer,
-                             ItemListSerializer, LotCreateSerializer)
-from api.utils import remove_leading_zeros
-
 
 class ItemViewset(GenericViewSet, mixins.CreateModelMixin):
     serializer_class = AcceptFileSerializer
     parser_classes = [MultiPartParser, JSONParser]
 
+    serializer_action_classes = {
+        'create': LotCreateSerializer,
+        'list_products': AcceptCodeSerializer,
+        'create_from_feed_file': AcceptFileSerializer
+    }
+
+    def get_serializer_class(self):
+        """ To retrun a specific serializer based on the action"""
+        try:
+            return self.serializer_action_classes[self.action]
+        except Exception:
+            return super().get_serializer_class()
+
     def get_queryset(self):
         Item.objects.all()
 
     def create(self, request, *args, **kwargs):
-        file_obj = request.data['file']
-        data = json.load(file_obj)
-        serializer = LotCreateSerializer(data=data['amounts'], many=True)
-        print(serializer.is_valid())
-        print(serializer.errors)
-        serializer.save()
+        serializer = LotCreateSerializer(data=request.data['amounts'], many=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
         return Response({'detail': 'recieved the file'})
 
-    @action(detail=False, methods=['post'], serializer_class=AcceptCodeSerializer)
+    # @action(detail=False, methods=['post'])
+    # def create_from_feed_file(self, request, *args, **kwargs):
+    #     file_obj = request.data['file']
+    #     data = json.load(file_obj)
+    #     serializer = LotCreateSerializer(data=data['amounts'], many=True)
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save()
+    #     return Response({'detail': 'recieved the file'})
+
+    @action(detail=False, methods=['post'])
+    def create_from_feed_file(self, request, *args, **kwargs):
+        file_obj = request.data['file']
+        data = json.load(file_obj)
+        serializer = SessionCreateSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response({'detail': 'recieved the file'})
+
+    @action(detail=False, methods=['post'])
     def list_products(self, request, *args, **kwargs):
         if request.data.get('code', None):
             code = remove_leading_zeros(request.data['code'])
